71ed431e1a706709f9803d7191c13ef8350792a5
Ingram <ingram.weeks@gmail.com>
2022-01-15 11:09:12 +0000

    more typing added and maximise images

--- photos.py
+++ photos.py
@@ -18,8 +18,8 @@ from typing import NewType
 
 from re import split
 
-photos_directory = '/home/ingram/Pictures/waifus/'
-cycle_period_ms = 30000
+photos_directory = '/home/ingram/Pictures/slideshow/'
+cycle_period_ms = 1000
 background_colour = "black"
 
 class ImageStore:
@@ -47,7 +47,7 @@ class ImageStore:
         return self.image_paths[self.index]
 
 class Application:
-    def __init__(self):
+    def __init__(self) -> None:
         """Setup the user interface here
         and initialize any dependent classes here
         """
@@ -62,30 +62,32 @@ class Application:
 
         self.window.update()
 
-    def increment_image(self):
+    def increment_image(self) -> None:
         """Get the next image in the directory and
         update the display, call self again in cycle_period_ms"""
         self.image = Image.open(photos_directory + self.store.next())
-        self.scale_image(self.image)
+        self.scale_image()
         self.photo_image = ImageTk.PhotoImage(self.image)
         self.display.configure(image = self.photo_image)
         self.window.after(cycle_period_ms, self.increment_image)
 
-    def scale_image(self, image) -> ImageTk:
+    def scale_image(self) -> ImageTk:
         """Get the size of the tkinter window - this
         will allow opened images to be scaled accordingly
         """
         window_size_raw = self.window.geometry()
         window_size = split("[x+]", window_size_raw)
-        image_size = image.size
+        width, height = self.image.size
 
-        self.image_scaling_factor = int(window_size[1])/image.size[1]
+        print(width, height)
 
-        x_size = int(self.image_scaling_factor * image_size[0])
-        y_size = int(self.image_scaling_factor * image_size[1])
+        self.image_scaling_factor = int(window_size[1])/height
+
+        x_size = int(self.image_scaling_factor * width)
+        y_size = int(self.image_scaling_factor * height)
 
         try:
-            self.image = image.resize((x_size, y_size))
+            self.image = self.image.resize((x_size, y_size))
         except ValueError:
             exit(1)
 
